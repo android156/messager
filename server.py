9c49efb8e7df9349a8a035215d2bd48e62dc91cb
android156 <n_degtyarev@mail.ru>
2022-09-08 18:22:30 +0300

    дескриптор заработал

--- server.py
+++ server.py
@@ -28,13 +28,30 @@ def arg_parser():
     return listen_address, listen_port
 
 
+# Дескриптор порта
 
+class PortDescriptor:
 
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.my_attr]
 
+    def __set__(self, instance, value):
+        if not 1023 < value < 65536:
+            logger.critical(
+                f'Попытка запуска сервера с указанием неподходящего порта {value}. '
+                f'Допустимы адреса с 1024 до 65535.')
+            raise ValueError(f'Попытка запуска клиента с неподходящим номером порта: {value}. '
+                             f'Допустимы адреса с 1024 до 65535. Сервер отключается.')
+        instance.__dict__[self.my_attr] = value
+
+    def __set_name__(self, owner, my_attr):
+        self.my_attr = my_attr
 
 
 # Основной класс сервера
 class Server(metaclass=ServerMaker):
+    port = PortDescriptor()
+
     def __init__(self, listen_address, listen_port):
         # Параметры подключения
         self.addr = listen_address
